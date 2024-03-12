a2ec0a7b31157fab98adcf3a3db2943218f9e8e9
Copium <119388242+Copiums@users.noreply.github.com>
2024-03-11 22:25:20 -0400

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -5901,3 +5901,22 @@ runFunction(function()
 	createKeystroke(Enum.KeyCode.D, UDim2.new(0, 76, 0, 42), UDim2.new(0, 8, 0, 5))
 	createKeystroke(Enum.KeyCode.Space, UDim2.new(0, 0, 0, 83), UDim2.new(0, 25, 0, -10))
 end)
+
+runFunction(function()
+        local KeyBindReminder = { Enabled = false }
+        KeyBindReminder = GuiLibrary.ObjectsThatCanBeSaved.UtilityWindow.Api.CreateOptionsButton({
+                Name = "Keybinds",
+                Function = function(callback)
+                        if callback then
+                                task.spawn(function()
+                                        repeat
+                                                warningNotification("Copium", "The Keybind for Infinity is: V", 60)
+                                                wait(60)
+                                                warningNotification("Copium", "Thank you for using Infinity", 30)
+						wait(30)				
+                                        until (not KeyBindReminder.Enabled)
+                                end)
+                        end
+                end
+        })
+end)
